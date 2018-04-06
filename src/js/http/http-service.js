59f7c4164d6a08d81c7d6843b7df02ac0ed172fc
avvet <sveta.drozdovich@yandex.ru>
2018-04-06 14:34:48 +0300

    Basic todo app - my attempt

--- src/js/http/http-service.js
+++ src/js/http/http-service.js
@@ -3,31 +3,30 @@ import axios from "axios";
 const MAIN_URL = 'http://localhost:3000/';
 const TODO = 'todo';
 
- class HttpService {
-    getTodoItems(callback){
-      axios.get(MAIN_URL + TODO)
-        .then(response => {
-          console.log(response.data);
-          callback(response.data);
-        })
-        .catch(error =>{
-          console.log(error);
-        })
-    }
-   postInputElements(newTodo){
-      axios.post(MAIN_URL + TODO,{
-        name: newTodo,
-        completed: 0
+class HttpService{
+  getAllTodos(callback){
+    axios.get(MAIN_URL + TODO)
+     .then(response => {
+      console.log(response.data);
+      callback(response.data);
+     })
+     .catch(error => {
+      console.log(error);
+     })
+  }
+  postFromInput(someName){
+    axios.post(MAIN_URL + TODO,{
+      name: someName,
+      completed: 0
+    })
+      .then(response => {
+        console.log(response.data);
       })
-        .then(response => {
-          console.log(response.data);
+      .catch(err => {
+        console.log(err);
+      })
+  }
+}
+let httpService = new HttpService();
+export{httpService}
 
-        })
-
-        .catch(error => {
-          console.log(error);
-        })
-   }
- }
- let httpService = new HttpService();
- export{httpService}
--- src/js/main.js
+++ src/js/main.js
@@ -5,36 +5,27 @@ import {printTodo} from "./helpers/todo-helper";
 
 let addAllItemsBtn = document.getElementById('main_btn');
 let takeContainer = document.getElementById('container');
-let AddButton = document.getElementById('post_btn');
+let addButton = document.getElementById('post_btn');
 let input = document.getElementById('my_input');
 
 addAllItemsBtn.onclick = () => {
-  httpService.getTodoItems((massiv) => {
-    massiv.map((value) => {
-      printItem(value.name);
+  httpService.getAllTodos((val) => {
+    val.map((oneVal) => {
+      makeTodoElem(oneVal.name);
     })
   });
-  // let printItem = (val) => {
-  //   let newTag = document.createElement('p');
-  //   newTag.innerHTML = val;
-  //   newTag.className = 'newTodoName';
-  //   takeContainer.appendChild(newTag);
-  // }
+};
+let makeTodoElem = (value) => {
+  let tag = document.createElement('p');
+  tag.innerHTML = value;
+  tag.className = 'new_class';
+  takeContainer.appendChild(tag);
 };
 
-let printItem = (val) => {
-  let newTag = document.createElement('p');
-  newTag.innerHTML = val;
-  newTag.className = 'newTodoName';
-  takeContainer.appendChild(newTag);
+addButton.onclick = () => {
+  let addValue = input.value;
+  makeTodoElem(addValue);
+  httpService.postFromInput(addValue);
+  input.innerHTML = '';
 };
 
-AddButton.onclick = () => {
-  let item = input.value;
-  if(item.length > 0){
-    printItem(item);
-    httpService.postInputElements(item);
-  }else{
-    alert('Enter todo Name');
-  }
-};
